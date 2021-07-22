00269dfb2459b0f450c9df354577f0aab0696ced
leonie-bu <leonie.buschbeck@gmail.com>
2021-07-22 15:50:56 +0200

    Create folder and files

---
+++ src/components/newtaskform/newtaskform.stories.js
@@ -0,0 +1,9 @@
+import "./newtaskform.css";
+import newtaskform from "./newtaskform.html";
+
+export default {
+  title: "Components/Newtaskform",
+  parameters: { layout: "fullscreen" },
+};
+
+export const NewTask = () => newtaskform;
