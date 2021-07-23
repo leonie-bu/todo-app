c38b4b129aad5bc9b6cf7f7efdaa7d68ebff134c
leonie-bu <leonie.buschbeck@gmail.com>
2021-07-23 13:50:34 +0200

    Create pages folder and addTask inside of this folder

---
+++ src/pages/addTask/addtask.stories.js
@@ -0,0 +1,9 @@
+import "./addtask.css";
+import addtask from "./positioning-button.html";
+
+export default {
+  title: "components/addTask",
+  parameters: { layout: "fullscreen" },
+};
+
+export const addTask = () => addTask;
