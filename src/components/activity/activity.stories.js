240a5bcced5f873d46eb1ad38c1252d70ead1fe7
leonie-bu <leonie.buschbeck@gmail.com>
2021-07-23 16:17:06 +0200

    Create activity folder in components

---
+++ src/components/activity/activity.stories.js
@@ -0,0 +1,9 @@
+import "./activity.css";
+import activity from "./activity.html";
+
+export default {
+  title: "Components/activity",
+  parameters: { layout: "fullscreen" },
+};
+
+export const Activity = () => activity;
