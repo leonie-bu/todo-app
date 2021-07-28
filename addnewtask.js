ab3e5e943137232505cf91ed54b7ec3a2e9cf343
leonie-bu <leonie.buschbeck@gmail.com>
2021-07-28 14:49:37 +0200

    Define form variable as a const variable

--- addnewtask.js
+++ addnewtask.js
@@ -1,14 +1,17 @@
-form.onsubmit = function (event {
+// Define what the form variable actually is
+const form = document.querySelector(".addTaskForm");
 
-    event.preventDefault();
+form.onsubmit = function (event) {
+  event.preventDefault();
 
-    const textInput = document.querySelector (".addTaskForm__textInput");
-    const taskName = textInput.value;
-    const checkedDateInput = document.querySelector (".addTaskForm__radio:checked");
-    const selectedDate = checkedDateInput.value;
-    console.log(selectedDate, taskName);
+  // Here we create a variable that trefers to what we type into the "Enter Task field --> chose the class from input element"
+  const textInput = document.querySelector(".addTaskForm__textInput");
+  //
+  const taskName = textInput.value;
 
-}
-
-
-
+  const checkedDateInput = document.querySelector(
+    ".addTaskForm__radio:checked"
+  );
+  const selectedDate = checkedDateInput.value;
+  console.log(selectedDate, taskName);
+};
