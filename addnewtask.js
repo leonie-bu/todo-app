a1dae745a09d62845c1a0ca3c2cecafc684bc204
leonie-bu <leonie.buschbeck@gmail.com>
2021-07-28 16:01:03 +0200

    Create object and replace two variable with the object key

--- addnewtask.js
+++ addnewtask.js
@@ -6,12 +6,15 @@ form.onsubmit = function (event) {
 
   // Here we create a variable that trefers to what we type into the "Enter Task field --> chose the class from input element"
   const textInput = document.querySelector(".addTaskForm__textInput");
-  //
-  const taskName = textInput.value;
 
   const checkedDateInput = document.querySelector(
     ".addTaskForm__radio:checked"
   );
-  const selectedDate = checkedDateInput.value;
-  console.log(selectedDate, taskName);
+
+  const newTask = { title: "", date: "", isDone: false };
+
+  newTask.title = textInput.value;
+  newTask.date = checkedDateInput.value;
+
+  console.log(newTask, newTask.title, newTask.date);
 };
